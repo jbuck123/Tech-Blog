9e7e9d515fa98a08e24a9b848baf85c1cd5f9248
JamesB <James.buchmann@gmail.com>
2022-08-07 01:28:20 -0500

    rendering the login and register page

--- server.js
+++ server.js
@@ -11,5 +11,11 @@ app.set('view engine', 'hbs')
 app.get('/', (req, res) => {
     res.render('index');
 });
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.get('/register', (req, res) => {
+    res.render('register');
+});
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
